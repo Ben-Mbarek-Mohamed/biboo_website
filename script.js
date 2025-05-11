fa225720a29aad7abcd90a22ce78d230b9642f6f
Ben-Mbarek-Mohamed <115542032+Ben-Mbarek-Mohamed@users.noreply.github.com>
2025-05-11 19:11:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("btnCommanderPrestige");
+    if (btn) {
+      btn.addEventListener("click", () => {
+        window.location.href = "/demande.html";  // Remplace par ta vraie URL
+      });
+    }
+  });
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("btnCommanderVisibilite");
+    if (btn) {
+      btn.addEventListener("click", () => {
+        window.location.href = "/demande.html";  // Remplace par ta vraie URL
+      });
+    }
+  });
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("btnCommanderDecouverte");
+    if (btn) {
+      btn.addEventListener("click", () => {
+        window.location.href = "/demande.html";  // Remplace par ta vraie URL
+      });
+    }
+  });
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("btnCommanderFidelite");
+    if (btn) {
+      btn.addEventListener("click", () => {
+        window.location.href = "/demande.html";  // Remplace par ta vraie URL
+      });
+    }
+  });
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("btnDedmandez");
+    if (btn) {
+      btn.addEventListener("click", () => {
+        window.location.href = "/demande.html";  // Remplace par ta vraie URL
+      });
+    }
+  });
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("btnCommencez");
+    if (btn) {
+      btn.addEventListener("click", () => {
+        window.location.href = "/packs.html";  // Remplace par ta vraie URL
+      });
+    }
+  });
